4f2ad1735bccf2dd8b4e1eb7315e6e27ee44405e
Pipuls <56545116+Pipuls@users.noreply.github.com>
2022-08-23 16:46:55 -0300

    correccion del system

--- ger/src/main/java/com/portfolioapbe/ger/GerApplication.java
+++ ger/src/main/java/com/portfolioapbe/ger/GerApplication.java
@@ -20,7 +20,8 @@ public class GerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolioapger.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -29,6 +30,5 @@ public class GerApplication {
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+		return new CorsFilter(urlBasedCorsConfigurationSource);	}
 }
